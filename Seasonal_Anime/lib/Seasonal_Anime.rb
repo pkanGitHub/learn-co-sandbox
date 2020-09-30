36cf42bd8d806b3f6b2858c220febea14d845aaa
Phyllis Kan <pyk9q7@gmail.com>
2020-09-30 07:15:24 +0000

    Automatically backed up by Learn

---
+++ Seasonal_Anime/lib/Seasonal_Anime.rb
@@ -0,0 +1,6 @@
+require "Seasonal_Anime/version"
+
+module SeasonalAnime
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ Seasonal_Anime/lib/Seasonal_Anime/version.rb
@@ -0,0 +1,3 @@
+module SeasonalAnime
+  VERSION = "0.1.0"
+end
